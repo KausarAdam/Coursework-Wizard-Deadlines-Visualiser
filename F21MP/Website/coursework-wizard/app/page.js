339540331c0c6e356a0aaa03d4a51b0feb33e1a1
Kausar Adam <62880903+KausarAdam@users.noreply.github.com>
2024-09-13 03:06:26 +0400

    Home Page (Complete)

--- F21MP/Website/coursework-wizard/app/page.js
+++ F21MP/Website/coursework-wizard/app/page.js
@@ -4,10 +4,22 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.homePage}>
+      <div className={styles.heading}>
+        <h2>M &nbsp;&nbsp;A &nbsp;&nbsp;S &nbsp;&nbsp;T &nbsp;&nbsp;E &nbsp;&nbsp;R &nbsp;&nbsp;&nbsp;&nbsp; Y &nbsp;&nbsp;O &nbsp;&nbsp;U &nbsp;&nbsp;R &nbsp;&nbsp;&nbsp;&nbsp; C &nbsp;&nbsp;O &nbsp;&nbsp;U &nbsp;R &nbsp;&nbsp;S &nbsp;&nbsp;E</h2>
+        <h1>D &nbsp;E &nbsp;A &nbsp;D &nbsp;L &nbsp;I &nbsp;N &nbsp;E &nbsp;S</h1>
+      </div>
+      <div className={styles.content}>
+        <p>Welcome to Coursework Wizard,</p>
+        <p>where visualising deadlines is</p>
+        <p>made simple and effective.</p>
+      </div>
+      <div className={styles.logIn}>
+        <button>Log In</button>
+      </div>
       <div className={styles.circle}></div>
       <div className={styles.alarmClock}>
         <Image
-          src="/alarm clock.jpg" // Path to your alarm clock image
+          src="/alarm clock.jpg" // Path to alarm clock image
           alt="Alarm Clock"
           layout="fill"
           objectFit="contain"
