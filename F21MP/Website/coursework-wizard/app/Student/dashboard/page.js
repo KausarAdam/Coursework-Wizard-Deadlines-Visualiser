cb66c2c836b0590a2567379d33c98c4464b07fc5
Kausar Adam <62880903+KausarAdam@users.noreply.github.com>
2024-09-18 02:16:53 +0400

    Fixed student menu overlap problem
    Added flexbox

--- F21MP/Website/coursework-wizard/app/Student/dashboard/page.js
+++ F21MP/Website/coursework-wizard/app/Student/dashboard/page.js
@@ -1,7 +1,22 @@
 import StudentMenu from "../student_menu";
+import styles from "./page.module.css";
 
 export default function Dashboard() {
     return (
-      <StudentMenu/>
+      <div className={styles.container}>
+        <StudentMenu/>
+
+        <div className={styles.dashboardPage}>
+          <h1 className={styles.heading}>Dashboard</h1>
+
+          <hr></hr>
+        
+          <h2 className={styles.subheading}>Hello, John Doe!</h2>
+
+          <div className={styles.box}></div>
+
+        </div>
+        
+      </div>
     );
   }
--- F21MP/Website/coursework-wizard/app/page.js
+++ F21MP/Website/coursework-wizard/app/page.js
@@ -5,35 +5,38 @@ import Header from "./Header.js";
 
 export default function Home() {
   return (
-    <div className={styles.homePage}>
+    <div>
       <Header/>
-      <div className={styles.heading}>
-        <h2>M &nbsp;&nbsp;A &nbsp;&nbsp;S &nbsp;&nbsp;T &nbsp;&nbsp;E &nbsp;&nbsp;R &nbsp;&nbsp;&nbsp;&nbsp; Y &nbsp;&nbsp;O &nbsp;&nbsp;U &nbsp;&nbsp;R &nbsp;&nbsp;&nbsp;&nbsp; C &nbsp;&nbsp;O &nbsp;&nbsp;U &nbsp;R &nbsp;&nbsp;S &nbsp;&nbsp;E</h2>
-        <h1>D &nbsp;E &nbsp;A &nbsp;D &nbsp;L &nbsp;I &nbsp;N &nbsp;E &nbsp;S</h1>
-        {/* &nbsp is for space */}
-      </div>
-      <div className={styles.content}>
-        <p>Welcome to Coursework Wizard,</p>
-        <p>where visualising deadlines is</p>
-        <p>made simple and effective.</p>
-      </div>
-      <div className={styles.logIn}>
-        <Link href="/Pre_logged_in/login_student" passHref>
-        <button>Log In</button>
-        </Link>
-      </div>
-      <div className={styles.circle}></div>
-      <div className={styles.alarmClock}>
-        <Image
-          src="/alarm clock.jpg"
-          alt="Alarm Clock"
-          layout="fill"
-          objectFit="contain"
-        />
-      </div>
-      <div className={styles.actNowText}>
-        <div>ACT -</div>
-        <div>NOW</div>
+
+      <div className={styles.homePage}>
+        <div className={styles.heading}>
+          <h2>M &nbsp;&nbsp;A &nbsp;&nbsp;S &nbsp;&nbsp;T &nbsp;&nbsp;E &nbsp;&nbsp;R &nbsp;&nbsp;&nbsp;&nbsp; Y &nbsp;&nbsp;O &nbsp;&nbsp;U &nbsp;&nbsp;R &nbsp;&nbsp;&nbsp;&nbsp; C &nbsp;&nbsp;O &nbsp;&nbsp;U &nbsp;R &nbsp;&nbsp;S &nbsp;&nbsp;E</h2>
+          <h1>D &nbsp;E &nbsp;A &nbsp;D &nbsp;L &nbsp;I &nbsp;N &nbsp;E &nbsp;S</h1>
+          {/* &nbsp is for space */}
+        </div>
+        <div className={styles.content}>
+          <p>Welcome to Coursework Wizard,</p>
+          <p>where visualising deadlines is</p>
+          <p>made simple and effective.</p>
+        </div>
+        <div className={styles.logIn}>
+          <Link href="/Pre_logged_in/login_student" passHref>
+          <button>Log In</button>
+          </Link>
+        </div>
+        <div className={styles.circle}></div>
+        <div className={styles.alarmClock}>
+          <Image
+            src="/alarm clock.jpg"
+            alt="Alarm Clock"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+        <div className={styles.actNowText}>
+          <div>ACT -</div>
+          <div>NOW</div>
+        </div>
       </div>
     </div>
   );
