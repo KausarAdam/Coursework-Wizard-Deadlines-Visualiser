3eac634339fe042d0511da2f8dd84f76e3070a76
Kausar Adam <62880903+KausarAdam@users.noreply.github.com>
2024-09-20 23:59:11 +0400

    Add copyright to footer

--- F21MP/Website/coursework-wizard/app/Student/Footer.js
+++ F21MP/Website/coursework-wizard/app/Student/Footer.js
@@ -7,7 +7,7 @@ export default function Footer() {
 
         <nav className={styles.nav}>
           <div className={styles.navLinks}>
-            <div className={styles.navDashboard}>Coursework Wizard</div>
+            <div className={styles.navDashboard}>&copy; {new Date().getFullYear()} Coursework Wizard</div>
             <a href ="/Student/help" className={styles.navLink}>Help</a>
             <a href="/Student/contact_us" className={styles.navLink}>Contact Us</a>
           </div>
